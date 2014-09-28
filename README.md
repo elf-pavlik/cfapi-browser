8d11ed704fa149019b530d0a7de4abca4c1516b5
elf Pavlik <perpetual-tripper@wwelves.org>
2014-09-28 13:28:02 +0200

    caching sort of works (db errors after ~3000 tripples)

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 # CFAPI Browser
 
-One of possible browser for Coder for All API
+One of many possible browsers for Coder for All API
+
+## aims
+
+Improve discovery of existing
+
+* Projects / Apps / Modules
+* Organizations / Initiatives
+* Civic hacking **hotspots**
+* Institutional Interfaces
+* Datasets **in use**
+
+## technologies
+
+* Cross Orgin Resource Sharing *[enable-cors.org](http://enable-cors.org)*, *[W3C spec](http://www.w3.org/TR/cors/)*
+* Linked Data *[intro video](http://youtu.be/4x_xzT5eF5Q)*,*[LOD Cloud](http://lod-cloud.net/)*, *[W3C glosary](http://www.w3.org/TR/ld-glossary/)*
+* RDF 1.1 *[primer](http://www.w3.org/TR/2014/NOTE-rdf11-primer-20140624/)*, *[W3C spec](http://www.w3.org/TR/rdf11-concepts/)*
+* JSON-LD *[intro video](http://youtu.be/vioCbTo3C-4)*, *[W3C spec](http://www.w3.org/TR/json-ld/)*
+* [JSON-LD Macros](https://github.com/antoniogarrote/json-ld-macros)
+* [Level Graph](http://levelgraph.io)
+
+## use cases
+
+Graph patterns allow sophisticated queries, similar to [facebook graph
+search](https://www.facebook.com/about/graphsearch).
+
+### during hackathon
+
+* show all the projects which participants of this event contribute to
+* show all the locations where participants of this event live in
+
+### anywhere
+
+* show all the apps which use this dataset (eg. local public transport
+  data)
+* show all the apps which connect to this public institution
+
--- app.js
+++ app.js
@@ -1,8 +1,99 @@
 $(function(){
 
   var entryPoint = 'http://codeforamerica.org/api/projects';
-  superagent.get(entryPoint)
-    .end(function(res){
-      console.log(res.body);
+  var graph = levelgraphJSONLD(levelgraph('abcdef'));
+
+  jsonld_macros.registerAPI({
+    "http://codeforamerica.org/api/projects/*": {
+      "$": {
+        "@context": { "@vocab": "http://schema.org/" },
+        "@type": "Code",
+        "@id": {"f:valueof": "api_url"},
+        "@only": ["name", "description", "url", "forksCount", "watchersCount",  "issuesCount"],
+        "@transform": {
+          "url": { "f:valueof": "code_url" },
+          "forksCount": [
+            { "f:valueof": "github_details" },
+            { "f:select": "forks_count"}
+          ],
+          "watchersCount": [
+            { "f:valueof": "github_details" },
+            { "f:select": "watchers_count"}
+          ],
+          "issuesCount": [
+            { "f:valueof": "github_details" },
+            { "f:select": "open_issues"}
+          ]
+        }
+      }
+    }
+  });
+
+  function getPagesCount(){
+    return new Promise(function(resolve, reject){
+      superagent.get(entryPoint)
+       .end(function(res){
+         if(!res.ok) reject(err);
+         var number = Number(res.body.pages.last.replace(entryPoint + '?page=', ''));
+         console.log('total pages', number);
+         resolve(number);
+       });
     });
+  }
+
+  function getPage(number){
+    return new Promise(function(resolve, reject){
+      superagent.get(entryPoint + '?page=' + number)
+        .end(function(res){
+          if(!res.ok) reject(err);
+          console.log('received page nr', number);
+          resolve(res.body);
+        });
+    });
+  }
+
+  function storeResource(resource){
+    return new Promise(function(resolve, reject){
+      graph.jsonld.put(resource, { "@context": resource["@context"] }, function(err, doc){
+        if(err) reject(err);
+        resolve();
+      });
+    });
+  }
+
+  function saveData(results){
+    var data = _.flatten(_.map(results, function(set){ return set.objects; }), true);
+    var filtered = _.filter(data, function(project){
+      return project.github_details;
+    });
+
+    var ld = _.map(filtered, function(project){
+       return jsonld_macros.resolve(project.api_url, project);
+    });
+
+    console.log(ld);
+
+    Promise.all(ld.map(storeResource))
+      .then(function(){
+        console.log('data saved to levelgraph');
+      }).catch(function(err){ console.log(err); });
+
+  }
+
+  var cache = function(){
+    getPagesCount().then(function(pagesCount){
+      Promise.all(_.range(1, pagesCount).map(getPage))
+        .then(saveData)
+        .catch(function(err){
+          console.log(err);
+        });
+    }).catch(function(err){ console.log(err); });
+  };
+
+  // debug
+  window.app = {
+    graph: graph,
+    cache: cache
+  };
+
 });
